b055d7f65e7f31ba407e801f97d4eff20886dc1d
XanderBurger <86130720+XanderBurger@users.noreply.github.com>
2024-04-19 14:00:40 -0600

    simple fix

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function () {
+export default function NotFound() {
   return (
     <div className='w-full h-[600px] grid items-center justify-center'>Sorry, Page Not Found.</div>
   )
