3c4fee999cb137669e325d417e709e12dc854d7e
Pinnow, Conrad <conrad.pinnow@montana.edu>
2024-09-19 10:38:16 -0600

    Revert "Update sanit-utils.js"
    This reverts commit 65b10c85fa2af1128986bbd1f65cd3268e9ddcb8.

--- sanity/sanit-utils.js
+++ sanity/sanit-utils.js
@@ -95,21 +95,6 @@ export function formatDate(dateString) {
     return formattedDate.replace(/(\d+)(,)/, `$1$2`);
 }
 
-const VideoPage = ({ videoUrl }) => (
-  <div>
-    <iframe
-      width="560"
-      height="315"
-      src={videoUrl}
-      frameBorder="0"
-      allow="autoplay; encrypted-media"
-      allowFullScreen
-    ></iframe>
-  </div>
-);
-
-export default VideoPage;
-
 const BLOGS =  groq`*[_type == "blogPost"] | order(_createdAt desc) {
     title,
     "image": coverImage.asset->url,
